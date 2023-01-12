d530fd2cfc1932998a163a110d99ad787d7738e1
u$f <usf.alami@gmail.com>
2023-01-12 14:10:30 +0100

    edit

--- src/main/java/org/usf/junit/addons/AssertExt.java
+++ src/main/java/org/usf/junit/addons/AssertExt.java
@@ -1,6 +1,7 @@
 package org.usf.junit.addons;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.function.Executable;
@@ -9,8 +10,12 @@ public final class AssertExt {
 	
 	private AssertExt() {}
 	
-	public static void assertThrowsMessage(String expectedMessage, Class<? extends Throwable> expectedType, Executable executable) {
+	public static void assertThrowsWithMessage(String expectedMessage, Class<? extends Throwable> expectedType, Executable executable) {
 		assertEquals(expectedMessage, assertThrows(expectedType, executable).getMessage());
+	}
+	
+	public static void assertThrowsWithCause(Throwable throwable, Class<? extends Throwable> expectedType, Executable executable) {
+		assertSame(throwable, assertThrows(expectedType, executable).getCause());
 	}
 
 }
